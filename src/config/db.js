1eeff9351cae9023941c19b979def543fd0473e4
ce2cs <boyang.gao@yahoo.com>
2021-08-21 23:36:28 -0700

    add mysql password

--- src/config/db.js
+++ src/config/db.js
@@ -21,10 +21,11 @@ if (isProd) {
     MYSQL_CONF = {
         host: 'localhost',
         user: 'root',
+        password: '123456',
         port: '3306',
         database: 'koa2_TwiDDer_db'
     }
-    
+
 }
 
 module.exports = {
